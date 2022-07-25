06abdb519a1ee89495c47f5889fc63c2980f4182
alimirzaei <mirzaeiali769@gmail.com>
2022-07-25 23:19:56 +0430

    use pragma once for fixing a bug

--- include/info.hpp
+++ include/info.hpp
@@ -1,4 +1,6 @@
-const unsigned int Width = 20;
-const unsigned int Height = 20;
+#pragma once
+
+const unsigned int Width = 21;
+const unsigned int Height = 23;
 const float CellSize = 30.f;
-const float Speed = 300.f;
+const float Speed = 2.f;

b3387d975ebf612c9e95740c1dc397d386093267
alimirzaei <mirzaeiali769@gmail.com>
2022-07-25 23:14:40 +0430

    add collision as a separate function for pacman and ghost

---
+++ include/Collision.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <array>
+#include <info.hpp>
+#include <SFML/Graphics.hpp>
+
+bool collision(float i_x, float i_y, std::array< std::array<sf::RectangleShape, Width>, Height> map);
---
+++ src/Collision.cpp
@@ -0,0 +1,56 @@
+#include <Collision.hpp>
+#include <cmath>
+
+using namespace std;
+using namespace sf;
+
+bool collision(float px, float py, array<array<RectangleShape, Width>, Height> map)
+{
+    float cell_x = px / static_cast<float>(CellSize);
+	float cell_y = py / static_cast<float>(CellSize);
+
+    for (int a = 0; a < 4; a++)
+	{
+	    float x = 0;
+		float y = 0;
+
+		switch (a)
+		{
+			case 0: //Top left cell
+			{
+				x = static_cast<float>(floor(cell_x));
+				y = static_cast<float>(floor(cell_y));
+
+				break;
+			}
+			case 1: //Top right cell
+			{
+				x = static_cast<float>(ceil(cell_x));
+				y = static_cast<float>(floor(cell_y));
+
+				break;
+			}
+			case 2: //Bottom left cell
+			{
+				x = static_cast<float>(floor(cell_x));
+				y = static_cast<float>(ceil(cell_y));
+
+				break;
+			}
+			case 3: //Bottom right cell
+			{
+				x = static_cast<float>(ceil(cell_x));
+				y = static_cast<float>(ceil(cell_y));
+			}
+		}
+
+		if (0 <= x && 0 <= y && Height > y && Width > x)
+		{
+            if (map[y][x].getFillColor() == Color::Blue)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}

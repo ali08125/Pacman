c524b04308d6e09defa6cc419aad8446dcde2778
alimirzaei <mirzaeiali769@gmail.com>
2022-07-25 14:31:48 +0430

    rename move to update in pacman class

--- include/Pacman.hpp
+++ include/Pacman.hpp
@@ -2,21 +2,21 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <info.hpp>
 
 class Pacman
 {
 private:
     sf::CircleShape player;
-    //sf::RectangleShape player;
     sf::Vector2f Pos;
     int dir;
-    int nextDir;
 
 
     void initPacman();
+    //bool collision(float i_x, float i_y, std::array<std::string, Height> sketch);
 
 public:
     Pacman();
     void draw(sf::RenderWindow & window);
-    void move(std::vector<sf::RectangleShape> Walls, float dt);
+    void update(std::vector<sf::RectangleShape> Walls, float dt, std::array<std::string, Height> sketch);
 };
--- src/Pacman.cpp
+++ src/Pacman.cpp
@@ -1,6 +1,6 @@
 #include <Pacman.hpp>
-#include <info.hpp>
 #include <iostream>
+#include <cmath>
 
 using namespace sf;
 
@@ -11,13 +11,12 @@ Pacman::Pacman()
 
 void Pacman::initPacman()
 {
-    Pos.x = 9 * CellSize;
-    Pos.y = 10 * CellSize;
-    player.setRadius(CellSize / 2 - 1);
-   // player.setSize(Vector2f(CellSize, CellSize));
+    Pos.x = 10 * CellSize;
+    Pos.y = 11 * CellSize;
+    player.setRadius(CellSize / 2);
     player.setFillColor(Color::Yellow);
     player.setPosition(Pos);
-    dir = 0;
+    dir = -1;
 }
 
 void Pacman::draw(RenderWindow & window)
@@ -25,63 +24,127 @@ void Pacman::draw(RenderWindow & window)
     window.draw(player);
 }
 
-void Pacman::move(std::vector<sf::RectangleShape> Walls, float dt)
+void Pacman::update(std::vector<sf::RectangleShape> Walls, float dt, std::array<std::string, Height> sketch)
 {
+   // std::cout << player.getPosition().x << "\n";
+    //std::cout << static_cast<float>(Speed * dt) << "\n";
+    /*
+    std::array<bool, 4> wall;
+    // Collision Right
+    wall[0] = collision(player.getPosition().x + (Speed * dt), player.getPosition().y, sketch);
+    // Collision Down
+    wall[1] = collision(player.getPosition().x, player.getPosition().y + (Speed * dt), sketch);
+    // Collision Left
+    wall[2] = collision(player.getPosition().x - (Speed * dt), player.getPosition().y, sketch);
+    // Collision Up
+    wall[3] = collision(player.getPosition().x, player.getPosition().y - (Speed * dt), sketch);
+*/
     Vector2f position;
-    FloatRect nextPos;
-    nextDir = dir;
+    position.x = 0;
+    position.y = 0;
 
     if(Keyboard::isKeyPressed(Keyboard::Right))
     {
-        dir = 1;
-        position.x += Speed * dt;
+        //if (wall[0] == 0)
+        {
+            dir = 0;
+        }   
     } else if(Keyboard::isKeyPressed(Keyboard::Down))
     {
-        dir = 2;
-        position.y += Speed * dt;
+        //if (wall[1] == 0)
+        {
+            dir = 1;
+        }
     } else if(Keyboard::isKeyPressed(Keyboard::Left))
-    {
-        dir = 3;
-        position.x -= Speed * dt;
+    {   
+        //if (wall[2] == 0)
+        {
+            dir = 2;
+        }
     } else if(Keyboard::isKeyPressed(Keyboard::Up))
     {
-        dir = 4;
-        position.y -= Speed * dt;
-    } else
-    {
-       // dir = 0;
+        //if (wall[3] == 0)
+        {
+            dir = 3;
+        }
+    }
+
+    //if (wall[dir] == 0)
+    { 
         switch (dir)
         {
-        case 1:
+        case 0:
             position.x += Speed * dt;
             break;
-        case 2:
+        case 1:
             position.y += Speed * dt;
             break;
-        case 3:
+        case 2:
             position.x -= Speed * dt;
             break;
-        case 4:
+        case 3:
             position.y -= Speed * dt;
             break;
         }
     }
-    // collision
-    for(auto & wall : Walls)
+    if (position.x == 0 && position.y == 0)
     {
-        FloatRect playerBounds = player.getGlobalBounds();
-        FloatRect wallBounds = wall.getGlobalBounds();
+    
+    } else
+        player.move(position.x, position.y);
+}
+/*
+bool Pacman::collision(float px, float py, std::array<std::string, Height> sketch)
+{
+    
+    float cell_x = px / static_cast<float>(CellSize);
+	float cell_y = py / static_cast<float>(CellSize);
 
-        nextPos = playerBounds;
-        nextPos.left += position.x;
-        nextPos.top += position.y;
+   // std::cout << cell_x << "\n";
+    for (int a = 0; a < 4; a++)
+	{
+	    float x = 0;
+		float y = 0;
 
-        if (wallBounds.intersects(nextPos))
-        {
-            position.x = 0;
-            position.y = 0;
-            
+		switch (a)
+		{
+			case 0: //Top left cell
+			{
+				x = static_cast<float>(floor(cell_x));
+				y = static_cast<float>(floor(cell_y));
+
+				break;
+			}
+			case 1: //Top right cell
+			{
+				x = static_cast<float>(ceil(cell_x));
+				y = static_cast<float>(floor(cell_y));
+
+				break;
+			}
+			case 2: //Bottom left cell
+			{
+				x = static_cast<float>(floor(cell_x));
+				y = static_cast<float>(ceil(cell_y));
+
+				break;
+			}
+			case 3: //Bottom right cell
+			{
+				x = static_cast<float>(ceil(cell_x));
+				y = static_cast<float>(ceil(cell_y));
+			}
+		}
+
+		if (0 <= x && 0 <= y && Height > y && Width > x)
+		{
+            //std::cout << "x : " << x << "\t y : " << y << "\n";
+            if (sketch[y][x] == '#')
+            {
+                return 1;
+            }
         }
     }
-    player.move(position.x, position.y);
+    return 0;
 }
+*/

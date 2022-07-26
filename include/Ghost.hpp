e7e194883df049203bfa8c3254f2102707a7cf0b
alimirzaei <mirzaeiali769@gmail.com>
2022-07-26 11:47:44 +0430

    add pacmanPos to update prototype

--- include/Ghost.hpp
+++ include/Ghost.hpp
@@ -7,11 +7,12 @@ class Ghost
 {
 private:
     sf::RectangleShape ghost;
+    int dir;
     
     void initGhosts();
 public:
     Ghost();
-    void update(std::array<std::array<sf::RectangleShape, Width>, Height> map);
+    void update(std::array<std::array<sf::RectangleShape, Width>, Height> map, sf::Vector2f pacmanPos);
     void draw(sf::RenderWindow &window);
 
     sf::RectangleShape getGhost() { return ghost; };
--- src/Ghost.cpp
+++ src/Ghost.cpp
@@ -10,12 +10,13 @@ Ghost::Ghost()
 
 void Ghost::initGhosts()
 {
+    dir = -1;
     ghost.setFillColor(Color::Red);
     ghost.setSize(Vector2f(CellSize, CellSize));
     ghost.setPosition(Vector2f(10 * CellSize, 7 * CellSize));
 }
 
-void Ghost::update(std::array<std::array<RectangleShape, Width>, Height> map)
+void Ghost::update(std::array<std::array<RectangleShape, Width>, Height> map, sf::Vector2f pacmanPos)
 {
     std::array<bool, 4> wall;
 
@@ -28,12 +29,66 @@ void Ghost::update(std::array<std::array<RectangleShape, Width>, Height> map)
     // Collision Up
     wall[3] = collision(ghost.getPosition().x, ghost.getPosition().y - (Speed), map);
 
-    if (wall[0] == 0)
+    Vector2f position;
+    position.x = 0;
+    position.y = 0;
+
+    if(Keyboard::isKeyPressed(Keyboard::D))
     {
-        ghost.move(0.2, 0); 
+        if (wall[0] == 0)
+        {
+            dir = 0;
+        }
+    } else if(Keyboard::isKeyPressed(Keyboard::S))
+    {
+        if (wall[1] == 0)
+        {
+            dir = 1;
+        }
+    } else if(Keyboard::isKeyPressed(Keyboard::A))
+    {   
+        if (wall[2] == 0)
+        {
+            dir = 2;
+        }
+    } else if(Keyboard::isKeyPressed(Keyboard::W))
+    {
+        if (wall[3] == 0)
+        {
+            dir = 3;
+        }
     }
-    
-    
+
+    if (wall[dir] == 0)
+    { 
+        switch (dir)
+        {
+        case 0:
+            position.x += Speed;
+            break;
+        case 1:
+            position.y += Speed;
+            break;
+        case 2:
+            position.x -= Speed;
+            break;
+        case 3:
+            position.y -= Speed;
+            break;
+        }
+    }
+    ghost.move(position.x, position.y);
+
+    if (ghost.getPosition().x >= CellSize * Width)
+    {
+        position.x = Speed - CellSize;
+        ghost.setPosition(Vector2f(position.x, ghost.getPosition().y));
+        
+    } else if (ghost.getPosition().x <= -CellSize)
+    {
+        position.x = Width * CellSize - Speed;
+        ghost.setPosition(Vector2f(position.x, ghost.getPosition().y));
+    }
 
 }
 

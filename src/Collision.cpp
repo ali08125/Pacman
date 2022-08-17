3d7fcdf4a836b4438323bff8712327eba261d779
alimirzaei <mirzaeiali769@gmail.com>
2022-08-17 23:28:39 +0430

    use a better way to detect wall collisions

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -6,55 +6,19 @@ using namespace sf;
 
 bool collision(float px, float py, array<array<RectangleShape, Width>, Height> map, int door)
 {
-    float cell_x = px / static_cast<float>(CellSize);
-	float cell_y = py / static_cast<float>(CellSize);
+	RectangleShape wall(Vector2f(CellSize, CellSize));
+	wall.setPosition(px, py);
 
-    for (int a = 0; a < 4; a++)
+	for (size_t i = 0; i < Width; i++)
 	{
-	    float x = 0;
-		float y = 0;
-
-		switch (a)
+		for (size_t j = 0; j < Height; j++)
 		{
-			case 0: //Top left cell
-			{
-				x = static_cast<float>(floor(cell_x));
-				y = static_cast<float>(floor(cell_y));
-
-				break;
-			}
-			case 1: //Top right cell
-			{
-				x = static_cast<float>(ceil(cell_x));
-				y = static_cast<float>(floor(cell_y));
-
-				break;
-			}
-			case 2: //Bottom left cell
-			{
-				x = static_cast<float>(floor(cell_x));
-				y = static_cast<float>(ceil(cell_y));
-
-				break;
-			}
-			case 3: //Bottom right cell
-			{
-				x = static_cast<float>(ceil(cell_x));
-				y = static_cast<float>(ceil(cell_y));
-			}
-		}
-
-		Vector2f block(10 * CellSize, 8 * CellSize);
-		if (0 <= x && 0 <= y && Height > y && Width > x)
-		{
-            if (map[y][x].getFillColor() == Color::Blue)
-            {
-                return 1;
-            } else if (map[y][x].getPosition() == block && door == 0)
+			if (wall.getGlobalBounds().intersects(map[j][i].getGlobalBounds())
+			&& map[j][i].getFillColor() == Color::Blue)
 			{
 				return 1;
 			}
-        }
-    }
-    return 0;
+		}
+	}
+	return 0;
 }
